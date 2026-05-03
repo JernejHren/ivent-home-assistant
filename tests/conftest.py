a5ec4522e6346d54133d16bd6c44fd2a38074d9e
JernejHren <106534210+JernejHren@users.noreply.github.com>
2026-05-03 08:47:58 +0200

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
+import pytest
 
+# aktivira pytest HA plugin
+pytest_plugins = "pytest_homeassistant_custom_component"
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations defined in this folder."""
+    yield
